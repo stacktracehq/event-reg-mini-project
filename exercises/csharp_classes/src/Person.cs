f141d68d5c3a0cb5244040caaad0ad5d18eb0354
Brad Bow <brad.bow@stacktracehq.com>
2019-09-07 15:23:13 +1000

    Adds empty Person class and test suite

---
+++ exercises/csharp_classes/src/Person.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Exercises
+{
+    public class Person
+    {
+        public Person(string name, DateTime dateOfBirth)
+        {
+        }
+
+        public int GetAgeInYears()
+        {
+            throw new NotImplementedException(nameof(GetAgeInYears));
+        }
+
+    }
+
+}
