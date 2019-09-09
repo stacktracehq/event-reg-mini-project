e9ea843c8574331472ad283a1c9799fc9414632d
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-09 14:46:43 +1000

    Books exercises seems to work

--- exercises/csharp_classes/src/Book.cs
+++ exercises/csharp_classes/src/Book.cs
@@ -5,24 +5,56 @@ namespace Exercises
     public class Book
     {
         public int CurrentPage { get; private set; }
+        public string Title { get; }
+        public string Author { get; }
+        public int NumberOfPages { get; }
 
         public Book(string title, string author, int numberOfPages)
         {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("A book must have a title");
+            }
+
+            if (string.IsNullOrEmpty(author))
+            {
+                throw new ArgumentException("A book must have an author");
+            }
+
+            if (numberOfPages <= 0)
+            {
+                throw new ArgumentException("A book must have at least one page");
+            }
+
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+            CurrentPage = 0;
         }
 
         public void SetBookmark(int pageNumber)
         {
-            throw new NotImplementedException(nameof(SetBookmark));
+            if (pageNumber < 1 || pageNumber > NumberOfPages)
+            {
+                throw new ArgumentException("Bookmarked page must be in range 1-" + NumberOfPages);
+            }
+            CurrentPage = pageNumber;
         }
 
         public void ClearBookmark()
         {
-            throw new NotImplementedException(nameof(ClearBookmark));
+            CurrentPage = 1;
         }
 
         public int EstimateRemainingReadingMinutes(double pagesReadPerMinute)
         {
-            throw new NotImplementedException(nameof(EstimateRemainingReadingMinutes));
+            int pagesLeftToRead = NumberOfPages - CurrentPage + 1;
+            if (pagesLeftToRead == 1 && pagesReadPerMinute > 1)
+            {
+                return 1;
+            }
+            double estimatedRemainingMinutes = pagesLeftToRead / pagesReadPerMinute;
+            return Convert.ToInt32(estimatedRemainingMinutes);
         }
     }
 
