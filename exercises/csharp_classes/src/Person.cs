a5e8531c06df611cfb16ce89d0784bb1e03b9dd0
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-09 13:53:47 +1000

    completed Person.cs

--- exercises/csharp_classes/src/Person.cs
+++ exercises/csharp_classes/src/Person.cs
@@ -4,13 +4,28 @@ namespace Exercises
 {
     public class Person
     {
+        public string Name {get;}
+        public DateTime DateOfBirth {get;}
         public Person(string name, DateTime dateOfBirth)
         {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("A person must have a name");
+            }
+
+            if (dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException("A person cannot have a date of birth in the future");
+            }
+
+            Name = name;
+            DateOfBirth = dateOfBirth;
         }
 
         public int GetAgeInYears()
         {
-            throw new NotImplementedException(nameof(GetAgeInYears));
+            return DateTime.Now.Year - DateOfBirth.Year;
+
         }
 
     }
