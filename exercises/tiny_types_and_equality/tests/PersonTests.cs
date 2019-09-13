cb6ccf7eb8d6469723bc8166b364dc8d6ce17696
Brad Bow <brad.bow@stacktracehq.com>
2019-09-13 16:02:13 +1000

    Uses tiny types in person impl
    This showed that a lot of our tests were not doing tedious
    data validation.

--- exercises/tiny_types_and_equality/tests/PersonTests.cs
+++ exercises/tiny_types_and_equality/tests/PersonTests.cs
@@ -7,54 +7,15 @@ namespace Tests
     public class PersonTests
     {
         private readonly DateTime _bradsBirthday = new DateTime(1987, 3, 6);
-        private readonly DateTime _colleensBirthday = new DateTime(1977, 3, 6);
-
-        [Fact]
-        public void CannotConstructAPersonWithNullName()
-        {
-            var exception = Assert.Throws<ArgumentException>(() => {
-                new Person(null, _bradsBirthday);
-            });
-
-            Assert.Contains(
-                "A person must have a name",
-                exception.Message
-            );
-        }
-
-        [Fact]
-        public void CannotConstructAPersonWithAnEmptyName()
-        {
-            var exception = Assert.Throws<ArgumentException>(() => {
-                new Person(string.Empty, _bradsBirthday);
-            });
-
-            Assert.Contains(
-                "A person must have a name",
-                exception.Message
-            );
-        }
-
-        [Fact]
-        public void CannotConstructAPersonWithADateOfBirthInTheFuture()
-        {
-            var exception = Assert.Throws<ArgumentException>(() => {
-                new Person("Brad", DateTime.Now.AddYears(1));
-            });
-
-            Assert.Contains(
-                "A person cannot have a date of birth in the future",
-                exception.Message
-            );
-        }
+        private readonly DateTime _colleensBirthday = new DateTime(1977, 3, 5);
 
         [Fact]
         public void AgeIsCalculatedCorrectlyForDatesInThePast()
         {
-            var brad = new Person("Brad", new DateTime(1987, 3, 6));
+            var brad = new Person(new Name("Brad"), new DateOfBirth( new DateTime(1987, 3, 6) ));
             Assert.Equal(32, brad.GetAgeInYears());
 
-            var colleen = new Person("Colleen", new DateTime(1977, 3, 6));
+            var colleen = new Person(new Name("Colleen"), new DateOfBirth(new DateTime(1977, 3, 6)));
             Assert.Equal(42, colleen.GetAgeInYears());
         }
     }
