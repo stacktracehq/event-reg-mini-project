859f66d35bdd1792c0ab676e3e09d38c515701fc
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-27 17:36:23 +1000

    adds EventManagementController and Tests

--- backend/src/Whiteboard.Registration.Web/Controllers/EventManagementController.cs
+++ backend/src/Whiteboard.Registration.Web/Controllers/EventManagementController.cs
@@ -3,49 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-// using Whiteboard.Registration.Domain;
+using Whiteboard.Registration.Domain;
+using Whiteboard.Registration.Domain.Tiny;
 
 namespace Whiteboard.Registration.Web.Controllers
 {
-    [Route("[controller]")]
+    public class EventManagementDto {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    // [Route("[controller]")]
+    [Route("v1/events")]
     [ApiController]
     public class EventManagementController : ControllerBase
     {
-        // GET /eventmanagement
+        public static List<EventManagementModel> Events { get; set; }  = new List<EventManagementModel>()
+            {
+                new EventManagementModel(
+                    Guid.NewGuid(),
+                    new EventTitle("Carlie's Amazing Event"),
+                    new EventDescription("a wonderful event"),
+                    new EventLocation("Stacktrace Headquarters"),
+                    new EventStartDate(new DateTime(2019, 09,30)),
+                    new EventEndDate(new DateTime(2019, 10, 20)),
+                    new RegistrationOpenDate(new DateTime(2019, 09, 22)),
+                    new RegistrationCloseDate(new DateTime(2019, 09, 29))),
+
+                new EventManagementModel(
+                    Guid.NewGuid(),
+                    new EventTitle("Another Great Thingy Event"),
+                    new EventDescription("here is the description"),
+                    new EventLocation("New Shanghai"),
+                    new EventStartDate(new DateTime(2020, 01, 13)),
+                    new EventEndDate(new DateTime(2020, 02, 01)),
+                    new RegistrationOpenDate(new DateTime(2019, 12, 13)),
+                    new RegistrationCloseDate(new DateTime(2020, 01, 07))),
+            };
+
+
+        // GET /v1/events/
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<EventManagementDto>> Get()
         {
-            // in here we display all the events
-             return new string[] { "value1", "value2" };
+            var listOfEvents = new List<EventManagementDto>();
+
+            foreach (var singleEvent in Events)
+            {
+                listOfEvents.Add(new EventManagementDto{
+                    Id  = singleEvent.Id,
+                    Title = singleEvent.Title.Value
+                });
+            }
+
+            Console.WriteLine(Events.Count);
+
+            return listOfEvents;
         }
 
-        // GET /eventmanagement/5
+        // GET /v1/events/{id}
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<EventManagementModel> Get(int id)
         {
-            // this is where we would display the details of one event
-             return "value";
+            // return Events[id].Title.Value.ToString();
+            return Events[id];
         }
 
-        // POST /eventmanagement
+        // POST /v1/events/
         [HttpPost]
-        public void Post([FromBody] string value)
+        // public void Post([FromBody] string value)
+        public void Post()
         {
             // create a new event
+            Events.Add(new EventManagementModel(
+                Guid.NewGuid(),
+                new EventTitle("Another Event"),
+                new EventDescription("Description here"),
+                new EventLocation("Awesome Location"),
+                new EventStartDate(new DateTime(2020,02,02)),
+                new EventEndDate(new DateTime(2020,03,02)),
+                new RegistrationOpenDate(new DateTime(2020,01,02)),
+                new RegistrationCloseDate(new DateTime(2020,01,30))));
+
+                Console.WriteLine(Events.Count);
         }
 
-        // PUT /eventmanagement/5
+        // PUT /v1/events/{id}
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // public void Put(int id, [FromBody] EventManagementModel value)
+        public void Put(int id, EventManagementModel value)
         {
-            // update the event
+            // Events[id] = new EventManagementModel(
+            //     value.Id,
+            //     value.Title,
+            //     value.Description,
+            //     value.EventLocation,
+            //     value.EventStartDate,
+            //     value.EventEndDate,
+            //     value.RegistrationOpenDate,
+            //     value.RegistrationCloseDate
+            // );
+
+            Events[id].Title = value.Title;
+            Events[id].Description = value.Description;
+            Events[id].EventLocation = value.EventLocation;
+            Events[id].EventStartDate = value.EventStartDate;
+            Events[id].EventEndDate = value.EventEndDate;
+            Events[id].RegistrationOpenDate = value.RegistrationOpenDate;
+            Events[id].RegistrationCloseDate = value.RegistrationCloseDate;
         }
 
-        // DELETE /eventmanagement/5
+        // DELETE /v1/events/{id}
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            // delete the individual event
+            Events.RemoveAt(id);
         }
     }
 }
---
+++ backend/tests/Whiteboard.Registration.Web.Tests/EventManagementControllerTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using Whiteboard.Registration.Web;
+
+namespace Whiteboard.Registration.Web.Tests
+{
+    public class EventManagementControllerTest
+    {
+        [Fact]
+        public void ThisIsATestTest()
+        {
+            Assert.Equal(2,2);
+        }
+    }
+}
