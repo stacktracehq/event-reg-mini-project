6e8ed73fda5ad87b7ec380d44808a1d56e6e2477
BlueCodeThree <carliemhamilton@gmail.com>
2019-10-04 14:56:36 +1000

    adds IEventManagemnetRepo to services

--- backend/src/Whiteboard.Registration.Web/Startup.cs
+++ backend/src/Whiteboard.Registration.Web/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Whiteboard.Registration.Web.Services;
 
 namespace Whiteboard.Registration.Web
 {
@@ -25,7 +19,10 @@ namespace Whiteboard.Registration.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddSingleton<IEventManagementRepo, InMemoryEventManagementRepo>()
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
