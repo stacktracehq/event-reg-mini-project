8564c741c37a56c1b2107e1c7626a02d7572ad3d
BlueCodeThree <carliemhamilton@gmail.com>
2019-10-25 10:40:18 +1000

    Form captures event name value
    Form shows the event name and outputs the value to the console

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -4,22 +4,63 @@ export interface Values {
     event_name: string;
 }
 
-export class Form extends React.Component {
+export interface FormState {
+    [key: string]: any;
+    values: Values[];
+    submitSuccess: boolean;
+}
+
+export class Form extends React.Component<{}, FormState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            event_name: ""
+            event_name: "",
+            values: [],
+            submitSuccess: false,
         }
     }
 
+    private processFormSubmission = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        const formData = {
+            event_name: this.state.event_name,
+        }
+        this.setState({ submitSuccess: true, values: [...this.state.values, formData]});
+        console.log("Event Name: " + this.state.event_name);
+    }
+
+    private handleInputChanges = (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        this.setState({
+            [e.currentTarget.name]: e.currentTarget.value,
+        })
+    }
+
     public render() {
+        const { submitSuccess} = this.state;
         return (
             <div>
                 <h2>Create a New Event</h2>
-                <form>
+                {!submitSuccess && (
+                      <div className="alert alert-info" role="alert">
+                          Fill the form below to create a new event
+                  </div>
+                  )}
+                  {submitSuccess && (
+                      <div className="alert alert-info" role="alert">
+                          The form was successfully submitted!
+                          </div>
+                  )}
+                <form onSubmit={this.processFormSubmission} noValidate={true}>
                     <div>
                         <label htmlFor="event_name">Event Name: </label>
-                        <input type="text" id="event_name" name="event_name" placeholder="Enter a title for your event" />
+                        <input
+                            type="text"
+                            id="event_name"
+                            onChange={(e) => this.handleInputChanges(e)}
+                            name="event_name"
+                            placeholder="Enter a title for your event"
+                        />
                     </div>
                     <div>
                         <button type="submit">
