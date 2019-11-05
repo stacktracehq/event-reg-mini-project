81f2afa034ff848f4f41e3535e34db60eaa1204a
BlueCodeThree <carliemhamilton@gmail.com>
2019-11-05 12:26:08 +1000

    adds simple AllEvents component

---
+++ frontend/src/components/AllEvents/AllEvents.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class AllEvents extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <div>
+                This will be all the events! And the home page
+            </div>
+        )
+    }
+}
+
+export default AllEvents;
