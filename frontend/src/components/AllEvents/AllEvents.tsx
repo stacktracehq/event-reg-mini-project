01532da89a34ec2fce8dc544860448ac974b609d
BlueCodeThree <carliemhamilton@gmail.com>
2019-11-06 09:36:22 +1000

    AllEvents.tsx now displays all events!

--- frontend/src/components/AllEvents/AllEvents.tsx
+++ frontend/src/components/AllEvents/AllEvents.tsx
@@ -1,10 +1,45 @@
 import React from "react";
+import { EventDTOs, EventDTO } from "../../models/index";
+import axios from "axios";
 
-export class AllEvents extends React.Component<{}, {}> {
+
+export class AllEvents extends React.Component<{}, EventDTOs> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            events: [ { id: "", title: "" } ]
+        }
+    }
+
+     componentDidMount() {
+        axios.request<EventDTO[]>({
+            url: 'https://localhost:5001/v1/events',
+
+        })
+            .then(response => {
+                const { data } = response;
+                this.setState({ events: data });
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     public render() {
         return (
             <div>
-                This will be all the events! And the home page
+                <h1>Carlie's Stupendous and Amazing Event Registration Project</h1>
+
+                    {this.state.events.map((anEvent) => {
+
+                        return (
+                            <ul key={anEvent.id}>
+                                <li>{anEvent.id}</li>
+                                <li>{anEvent.title}</li>
+                            </ul>
+                        )
+                    })}
+
             </div>
         )
     }
