940dad78286575c7a451dd047c2d5fc0692db229
BlueCodeThree <carliemhamilton@gmail.com>
2019-11-05 12:26:56 +1000

    adds routing to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import {
+    Route,
+    Link,
+    BrowserRouter as Router
+} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+import Form from "./components/Form/Form";
+import AllEvents from "./components/AllEvents/AllEvents";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/new">Create New Event</Link>
+                </li>
+            </ul>
+            <Route exact path="/" component={AllEvents} />
+            <Route path="/new" component={Form} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
